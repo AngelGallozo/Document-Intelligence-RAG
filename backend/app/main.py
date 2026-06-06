701967e029c07ad9a534f41993913b2955a538db
AngelGallozo <71468166+AngelGallozo@users.noreply.github.com>
2026-06-06 14:34:02 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.documents import router as documents_router
 
 app = FastAPI(
@@ -10,4 +10,14 @@ app.include_router(
     documents_router,
     prefix="/api",
     tags=["Documents"]
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
